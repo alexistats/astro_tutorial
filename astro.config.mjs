9078a02534f6a6a703d997a7cbbc9799bb7e0437
alexis <git@alexistats.com>
2023-12-03 16:39:17 -0500

    Added tags

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,9 @@
 import { defineConfig } from 'astro/config';
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
 export default defineConfig({
-    site: "https://alexistats.netlify.app/"
+  site: "https://alexistats.netlify.app/",
+  integrations: [preact()]
 });
